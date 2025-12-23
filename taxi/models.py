8dfd678ab009a361f2f381fd65e497d49574a5f1
Ann Riabykina <anny.r20.1994@gmail.com>
2025-12-23 20:44:47 +0200

    Remove db.sqlite3 and __pycache__ from repository

--- taxi/models.py
+++ taxi/models.py
@@ -17,8 +17,15 @@ class Manufacturer(models.Model):
 
 class Car(models.Model):
     model = models.CharField(max_length=50)
-    manufacturer: Manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    drivers: models.ManyToManyField = models.ManyToManyField(Driver)
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.CASCADE
+        related_name="cars"
+    )
+    drivers = models.ManyToManyField(
+        Driver,
+        related_name="cars"
+    )
 
     def __str__(self):
         return f"{self.model} ({self.manufacturer.name})"
